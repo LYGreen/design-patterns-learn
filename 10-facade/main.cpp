c42f96f403314b75045c56edfbc855a7f64df493
LYGreen <3098036603@qq.com>
2025-05-25 22:42:01 +0800

    upload files

---
+++ 10-facade/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string.h>
+
+class VideoFile {
+
+};
+
+class OggCompressionCodec {
+
+};
+
+class MPEG4CompressionCodec {
+
+};
+
+class CodecFactory {
+
+};
+
+class BitrateReader {
+
+};
+
+class AudioMixer {
+
+};
+
+class VideoConverter {
+public:
+    void convert() {
+        // ...
+    }
+};
+
+int main(int argc, char* argv[]) {
+    // 太多了不写了
+    
+    return 0;
+}
